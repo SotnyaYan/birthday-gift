c6d0da9b6d79fd64944b1cd564e0013ec3777472
SotnyaYan <yansotnya@gmail.com>
2026-08-04 01:27:37 +0300

    таймер

--- src/utils/timer.js
+++ src/utils/timer.js
@@ -1,6 +1,6 @@
-//export const HOUR = 60 * 60 * 1000;
+export const HOUR = 2 * 60 * 60 * 1000;
 // Для тестов потом можно заменить на:
-export const HOUR = 10 * 1000;
+// export const HOUR = 10 * 1000;
 
 export function nextUnlock() {
     return Date.now() + HOUR;
